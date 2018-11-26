b1b6801e6ebbc81d87b679e3def35044b4fb9e37
santosh <santosh.ece06@gmail.com>
2018-11-26 14:39:43 +0530

    Update.

--- App.js
+++ App.js
@@ -19,9 +19,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 50,
     textAlign: 'center',
     margin: 10,
+    fontFamily: 'cursive'
   },
   instructions: {
     textAlign: 'center',
