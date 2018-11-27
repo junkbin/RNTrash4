9a98fa37559d0bdbca6a9a157aef56cf74ead507
santosh <santosh.ece06@gmail.com>
2018-11-27 12:59:09 +0530

    New Web View added.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
-import {WebViewDemo1 as appRootComp} from './src';
+import {WebViewNew as appRootComp} from './src';
 AppRegistry.registerComponent(appName, () => appRootComp);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {Hello} from './Hello';
 import {WebViewDemo} from './WebViewDemo';
 import {WebViewDemo1} from './WebViewDemo1';
+import {WebViewNew} from './WebViewNew';
 
-export {Hello, WebViewDemo, WebViewDemo1}
+export {Hello, WebViewDemo, WebViewDemo1, WebViewNew}
