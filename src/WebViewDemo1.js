88eb8579fdf7a6388af5a3048bbc4ad13340e547
santosh <santosh.ece06@gmail.com>
2018-11-27 11:06:30 +0530

    WebViewDemo1

---
+++ src/WebViewDemo1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {WebView, Text, StyleSheet} from 'react-native';
+
+export class WebViewDemo1 extends React.Component{
+    render(){
+        return (
+            <WebView
+                originWhitelist={['*']}
+                source={{ html: '<h3>WebViewDemo1</h3><iframe width="100%" height="300" src="http://jsfiddle.net/viveking/gxvu5r6s/2/embedded/result/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe> <h3>WebviewDemo1</h3>' }}
+            />
+        );
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {Hello} from './Hello';
 import {WebViewDemo} from './WebViewDemo';
+import {WebViewDemo1} from './WebViewDemo1';
 
-export {Hello, WebViewDemo}
+export {Hello, WebViewDemo, WebViewDemo1}
